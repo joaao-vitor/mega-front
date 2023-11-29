131ba611677fcf640e907f15a6ea09e5c8b0920c
João Vitor <jvitor.arrudal@gmail.com>
2023-11-29 03:30:15 -0300

    fix verify

--- src/views/VerifyEmailView.vue
+++ src/views/VerifyEmailView.vue
@@ -1,5 +1,5 @@
 <script>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import Button from '../components/Button.vue'
 import axios from 'axios'
